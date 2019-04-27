4257401516d243bae19856342f7d6a6a0e26533c
Raghukul Raman <raghukul.raman01@gmail.com>
2019-04-27 19:32:11 +0530

    Add some changes in graph plotting library

--- experiments/plotting/gen_plot.py
+++ experiments/plotting/gen_plot.py
@@ -30,20 +30,69 @@ X = 0.32, y = 9315.19
 # X = [0.16, 0.20, 0.24, 0.28, 0.32, 0.36, 0.40]
 # Y = [49739.76, 23595.67, 14041.67, 10144.72, 9315.19, 9010.58, 8982.42]
 
-plt.plot(X, Y)
+#fig5
+# Different queries on a cluster of 6 nodes
+# X = [10, 100, 1000, 10000, 100000, 1000000]
+# Y = [1.05, 8.86, 77.09, 874.78, 8052.35, 91247.44]
 
+# fig6
+# variation of response time with the number of nodes in cluster
+# Number of queries kept fixed - 10^5 queries
+# X = [1, 2, 3, 4, 5]
+# Y = [7681.32, 7735.65, 7553.48, 7783.68, 7868.42]
+
+#fig7
+# variation of down time in different cluster configs
+# number of keys stored in system is 10^5
+# X = [2, 3, 4, 5, 6]
+# Y = [3289.17, 2336.22, 1401.345, 1218.66, 991.40]
+
+
+#fig8
+# variation of number of keys, varying number of nodes also.
+
+# plt.plot(X, Y, label='cluster')
 # plt.xscale('log')
 
-plt.ylabel('response time (ms)')
-plt.xlabel('number of queries (set) - logarithmic scale')
-plt.suptitle('Single redis instance, with no memory/cpu restriction', fontsize=16)
+x1 = [1, 1.1]
+y1 = [9581.98, 68284.44]
 
+x2 = [1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7]
+y2 = [9489.88, 10223.69, 11295.78, 13280.35, 14054.00, 14698.6, 78593.78]
+
+x3 = [1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3]
+y3 = [14690.53, 15579.69, 16842.89, 17425.28, 18263.41, 19195.42, 101342.3]
+
+x4 = [2.3, 2.4]
+y4 = [19753.17, 21329.86]
+
+plt.plot(x1, y1)
+plt.plot(x2, y2)
+plt.plot(x3, y3)
+plt.plot(x4, y4)
+
+
+plt.ylabel('avg. time (ms)')
+plt.xlabel('number of queries (x * 10^5)')
+plt.suptitle('Varying queries and number of nodes', fontsize=16)
+# plt.legend(loc='upper left')
 # plt.xticks([0.12, 0.14, 0.16, 0.2, 0.24, 0.28, 0.32, 0.36, 0.40, 0.44, 0.48])
 
-for i in range(len(X)):
-    coordinates = '('+str(X[i]) +', '+str(Y[i])+')'
-    plt.text(X[i], Y[i], coordinates, fontsize=9)
-    plt.scatter(X[i], Y[i], c="r",marker='o')
+for i in range(len(x1)):
+    coordinates = '('+str(x1[i]) +', '+str(y1[i])+')'
+    plt.scatter(x1[i], y1[i], c="r",marker='o')
+for i in range(len(x2)):
+    coordinates = '('+str(x2[i]) +', '+str(y2[i])+')'
+    plt.scatter(x2[i], y2[i], c="r",marker='x')
+for i in range(len(x3)):
+    coordinates = '('+str(x3[i]) +', '+str(y3[i])+')'
+    plt.scatter(x3[i], y3[i], c="r",marker='*')
+for i in range(len(x4)):
+    coordinates = '('+str(x4[i]) +', '+str(y4[i])+')'
+    plt.scatter(x4[i], y4[i], c="r",marker='+')
+
+
+
 
 
 # plt.show()
